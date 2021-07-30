526cc7568c07776c314a63d4e591baafd891cb8c
Sérgio <sergiogabrielb23@gmail.com>
2021-07-30 17:20:20 -0300

    a'

--- edit.php
+++ edit.php
@@ -55,52 +55,85 @@ $res = $resultados->fetch(PDO::FETCH_ASSOC);
 
 <head>
 	<title>Editar produto</title>
+	<link rel="preconnect" href="https://fonts.googleapis.com" />
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+	<link href="https://fonts.googleapis.com/css2?family=MonteCarlo&display=swap" rel="stylesheet" />
+	<link rel="preconnect" href="https://fonts.googleapis.com" />
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+	<link href="https://fonts.googleapis.com/css2?family=Gowun+Dodum&display=swap" rel="stylesheet" />
+	<link href="./styles/styles.css" rel="stylesheet" type="text/css" />
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css" />
 </head>
 
 <body>
-	<a href="manipularProduto.php">Voltar para edição de produtos</a>
-	<br /><br />
+	<div class="menu">
+		<h3><i class="fa fa-mobile" aria-hidden="true"></i>EletronicBuy</h3>
+		<a class="home" href="index.php"><i class="fa fa-home" aria-hidden="true"></i>Pagina Inicial</a>
+	</div>
+	<div class="content">
+		<img class="imgfundo" src="./images/background.jpg" />
+		<div class="conteudo2">
+			<div class="conteudoCadastro2">
+				<div class="titulos">
+					<h1 class="tituloCadastro">Edição de produtos</h1>
+					<a class="icon" href="manipularProduto.php"><i class="fas fa-long-arrow-alt-left"></i>
 
-	<form name="form1" method="post" action="edit.php">
-		<table border="0">
-			<tr>
-				<td>Nome do produto</td>
-				<td><input type="text" name="titulo" value="<?php echo $res['titulo']; ?>"></td>
-			</tr>
-			<tr>
-				<td>Descricao</td>
-				<td><input type="text" name="descricao" value="<?php echo $res['descricao']; ?>"></td>
-			</tr>
-			<tr>
-				<td>Preço</td>
-				<td><input type="text" name="preco" value="<?php echo  $res['preco']; ?>"></td>
-			</tr>
-			<tr>
-				<td>Porcentagem de desconto</td>
-				<td><input type="text" name="porc_desconto" value="<?php echo  $res['porc_desconto']; ?>"></td>
-			</tr>
-			<tr>
-				<td>Estoque</td>
-				<td><input type="text" name="estoque" value="<?php echo  $res['estoque']; ?>"></td>
-			</tr>
-			<tr>
-				<td>Situacao</td>
-				<td>
-					<select name="situacao" id="">
-						<option value="disponivel" name="disponivel">disponivel</option>
-						<option value="indisponivel" name="indisponivel">
-							indisponivel
-						</option>
-						<option value="oferta" name="oferta">oferta</option>
-					</select>
-					<input type="file" />
-				</td>
-			</tr>
-			<tr>
-				<td><input type="hidden" name="id" value=<?php echo $_GET['id']; ?>></td>
-				<td><input type="submit" name="update" value="Atualizar"></td>
-			</tr>
-		</table>
+						Voltar para edição de produtos</a>
+				</div>
+
+
+				<form class="formCadastro" name="form1" method="post" action="edit.php">
+					<div class="inputsCadastro">
+						<div class="imagem">
+							<script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
+							<lottie-player src="https://assets2.lottiefiles.com/packages/lf20_xjhwr9wv.json" background="transparent" speed="1" style="width: 600px; height: 600px" loop autoplay></lottie-player>
+						</div>
+						<div class="inputs">
+							<div class="info">Insira as informações do produto</div>
+							<div class="nomeProduto">
+								<p class="nameProduto">Nome do produto:</p>
+								<input type="text" name="titulo" />
+							</div>
+							<div class="nomeProduto">
+								<p class="descProduto">Descrição:</p>
+								<input type="text" name="descricao" />
+							</div>
+							<div class="nomeProduto">
+								<p class="selectProduto">Porcentagem Desconto:</p>
+								<input type="text" name="descricao" />
+							</div>
+							<div class="nomeProduto">
+								<p>Preço:</p>
+								<input type="text" name="preco" />
+							</div>
+							<div class="nomeProduto">
+								<p class="estoProduto">Estoque:</p>
+								<input type="text" name="estoque" />
+							</div>
+							<div class="nomeProduto">
+								<p class="descProduto">Situação:</p>
+								<select class="selectCadastro" name="situacao" id="">
+									<option value="disponivel" name="disponivel">
+										Disponível
+									</option>
+									<option value="indisponivel" name="indisponivel">
+										Indisponivel
+									</option>
+									<option value="oferta" name="oferta">Oferta</option>
+								</select>
+							</div>
+							<div class="nomeProduto">
+								<p class="selectProduto">Selecione a foto do produto:</p>
+								<input class="file" type="file" required name="file" />
+							</div>
+							<div class="botaoCadastrar">
+								<input class="buttonCadastro" type="submit" name="Submit" value="Adicionar" />
+							</div>
+						</div>
+					</div>
+			</div>
+		</div>
+	</div>
 	</form>
 </body>
 
